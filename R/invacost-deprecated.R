e626399606d94be9d07db735d2efee103e72249d
Boris Leroy <leroy.boris@gmail.com>
2024-02-11 07:40:02 +0000

    version 1.1-6

--- R/invacost-deprecated.R
+++
@@ -1,9 +0,0 @@
-## invacost-deprecated.r
-#' @title Deprecated functions in package \pkg{invacost}.
-#' @description The functions listed below are deprecated and will be defunct in
-#'   the near future. Alternative functions with similar
-#'   functionality are provided. Help pages for deprecated functions are
-#'   available at \code{help("<function>-deprecated")}.
-#' @name invacost-deprecated
-#' @keywords internal
-NULL
